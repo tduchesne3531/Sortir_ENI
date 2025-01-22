3fe838d201fe39947ea97c3e927c456ed9f3886e
Karl <karl.delourmel@gmail.com>
2025-01-22 15:21:44 +0100

    front tbl + filter

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Activity;
+use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
+use App\Service\ActivityService;
+use App\Service\SiteService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,26 +16,38 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ActivityController extends AbstractController
 {
 
-    private SortieRepository $sortieRepository;
     private SiteRepository $siteRepository;
+    private ActivityService $activityService;
+    private SiteService $siteService;
+    private ParticipantRepository $participantRepository;
 
-    public function __construct(SortieRepository $sortieRepository, SiteRepository $siteRepository)
+    /**
+     * @param SiteRepository $siteRepository
+     * @param ActivityService $activityService
+     * @param SiteService $siteService
+     * @param ParticipantRepository $participantRepository
+     */
+    public function __construct(SiteRepository $siteRepository, ActivityService $activityService, SiteService $siteService, ParticipantRepository $participantRepository)
     {
-        $this->sortieRepository = $sortieRepository;
         $this->siteRepository = $siteRepository;
+        $this->activityService = $activityService;
+        $this->siteService = $siteService;
+        $this->participantRepository = $participantRepository;
     }
+
 
     #[Route('/activity', name: 'app_all_activityies', methods: ['GET'])]
     public function getAll(): Response
     {
-        $activities = $this->sortieRepository->findAll();
         $user = $this->getUser();
-        $sites = $this->siteRepository->findAll();
+        $participant = $user ? $this->participantRepository->find(['email' => $user->getUserIdentifier()]) : null;
+        $activities = $this->activityService->getAll();
+        $sites = $this->siteService->findAllSites();
 
-        return $this->render('activity/activity.html.twig', [
+        return $this->render('activity/activities.html.twig', [
             'controller_name' => 'ActivityController',
             'activities' => $activities,
-            'user' => $user,
+            'participant' => $participant,
             'sites' => $sites,
         ]);
     }
@@ -40,15 +56,29 @@ final class ActivityController extends AbstractController
     public function getAllBySite(int $siteId): Response
     {
         $site = $this->siteRepository->find($siteId);
-        $activities = $this->sortieRepository->findBy(['site' => $site], ['date' => 'DESC']);
+        $activities = $this->activityService->getAllBySite($site);
         $user = $this->getUser();
         $sites = $this->siteRepository->findAll();
 
-        return $this->render('activity/activity.html.twig', [
+        return $this->render('activity/activities.html.twig', [
             'controller_name' => 'ActivityController',
             'activities' => $activities,
             'user' => $user,
             'sites' => $sites,
         ]);
     }
+
+    #[Route('/activity/{activityId}', name: 'app_activity', methods: ['GET'])]
+    public function get(int $activityId): Response
+    {
+        $activity = $this->activityService->get($activityId);
+        $user = $this->getUser();
+
+        return $this->render('activity/activity.html.twig', [
+            'controller_name' => 'ActivityController',
+            'activity' => $activity,
+            'user' => $user
+        ]);
+    }
+
 }
